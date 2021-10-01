11ac1d14fde2fa4f1e910e1beef26fb2dc7e56e4
Cameron Romo <cromo1225@gmail.com>
2021-10-01 16:03:54 -0600

    Add landing page for the app

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,26 +1,43 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const HomePage = () => {
   return (
     <SafeAreaView>
-      {/* <Image
-        source={require('../../assets/img/DJ Night owl.png')}
-        style={styles.image}
-      /> */}
-      <Text>Hello World</Text>
+      <Image source={require('../../assets/img/logo.png')} style={styles.image} />
+      <View>
+        <TouchableOpacity style={styles.enterButton}>
+          <Text style={styles.buttonText}>Bump the Base!</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonText: {
+    color: 'white',
+  },
+  enterButton: {
+    backgroundColor: 'royalblue',
+    position: 'absolute',
+    alignSelf: 'center',
+    top: -200,
+    width: 200,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
   image: {
-    flex: 1,
-    height: 190,
-    width: 140,
-    resizeMode: 'contain',
-  }
-})
+    alignSelf: 'center',
+    justifyContent: 'flex-end',
+    height: 760,
+    width: 540,
+    marginTop: -50,
+    resizeMode: 'stretch',
+  },
+});
 
-export default HomePage
+export default HomePage;
